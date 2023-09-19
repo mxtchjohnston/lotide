83dda88831bae0029c820a69668830fbc9669199
mxtchjohnston <mitchjohnston99@gmail.com>
2023-09-19 04:39:49 +0000

    without.js

--- eqArrays.js
+++ eqArrays.js
@@ -29,8 +29,11 @@ const assertArraysEqual = function(actual, expected) {
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
-assertArraysEqual([1, 2, 3], [3, 2, 1]) // => false
+// assertArraysEqual([1, 2, 3], [1, 2, 3]) // => true
+// assertArraysEqual([1, 2, 3], [3, 2, 1]) // => false
 
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]) // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]) // => false
+// assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]) // => true
+// assertArraysEqual(["1", "2", "3"], ["1", "2", 3]) // => false
+
+exports.assertArraysEqual = assertArraysEqual;
+exports.isEqual = isEqual;

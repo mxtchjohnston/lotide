8d2e5a63b9edd453c7f1a42956f4f84592f21230
mxtchjohnston <mitchjohnston99@gmail.com>
2023-09-20 19:36:36 +0000

    middle impl

--- middle.js
+++ middle.js
@@ -5,16 +5,24 @@ const { assertArraysEqual } = require("./eqArrays.js");
 //Arrays that are odd should return the middle most element
 
 const middle = (array) => {
-  let newArray = [];
-  const middle = Math.floor(array.length / 2);
-  console.log(array.length, middle);
+  // let newArray = [];
+  // const middle = Math.floor(array.length / 2);
+  // console.log(array.length, middle);
 
-  if (array.length <= 2) {
-    return []
-  } else if (array.lenth % 2 === 0) {
-    return [array[middle - 2], array[middle - 1]];
+  // if (array.length <= 2) {
+  //   return []
+  // } else if (array.lenth % 2 === 0) {
+  //   return [array[middle - 2], array[middle - 1]];
+  // } else {
+  //   return [array[middle]]; 
+  // }
+  const middle = Math.floor(array.length / 2);
+  if (array.length < 3) {
+    return [];
+  } else if (array.length % 2 === 1) {
+    return [array[middle]];
   } else {
-    return [array[middle]]; 
+    return [array[middle - 1], array[middle]];
   }
 }
 

8872a229af155ef1c24ce5d92a36190df20df46c
mxtchjohnston <mitchjohnston99@gmail.com>
2023-09-20 03:57:10 +0000

    naive implementation didnt work, imagine that

---
+++ eqObjects.js
@@ -0,0 +1,16 @@
+const { assertEqual } = require('./assertEqual.js');
+const { isEqual } = require('./eqArrays.js');
+
+const eqObjects = (obj1, obj2) => {
+  return isEqual(Object.entries(obj1), Object.entries(obj2));
+}
+
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject= { size: "medium", color: "red" };
+eqObjects(shirtObject , anotherShirtObject); // => true
+//We need to use that return value in combination with assertEquals to test if the function is working correctly.
+assertEqual(eqObjects(shirtObject , anotherShirtObject), true);
+
+const longSleeveShirtObject= { size: "medium", color: "red", sleeveLength: "long" };
+eqObjects(shirtObject , longSleeveShirtObject); // => false
+assertEqual(eqObjects(shirtObject , longSleeveShirtObject), false);

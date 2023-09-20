36e8ce84c0c2e67a0e1798acf89feb2491e2b98b
mxtchjohnston <mitchjohnston99@gmail.com>
2023-09-20 07:41:04 +0000

    cant do object to object

--- eqObjects.js
+++ eqObjects.js
@@ -2,15 +2,28 @@ const { assertEqual } = require('./assertEqual.js');
 const { isEqual } = require('./eqArrays.js');
 
 const eqObjects = (obj1, obj2) => {
-  return isEqual(Object.entries(obj1), Object.entries(obj2));
-}
+  let entries1 = Object.entries(obj1);
+  let entries2 = Object.entries(obj2);
+
+  if (entries1.length !== entries2.length) {
+    return false;
+  }
+
+  for (let [key, value] of entries1) {
+    if (!isEqual(obj2[key], value)) {
+      return false;
+    }
+  }
+  return true;
+};
 
 const shirtObject = { color: "red", size: "medium" };
-const anotherShirtObject= { size: "medium", color: "red" };
+const anotherShirtObject = { size: "medium", color: "red" };
 eqObjects(shirtObject , anotherShirtObject); // => true
+
 //We need to use that return value in combination with assertEquals to test if the function is working correctly.
 assertEqual(eqObjects(shirtObject , anotherShirtObject), true);
 
-const longSleeveShirtObject= { size: "medium", color: "red", sleeveLength: "long" };
+const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
 eqObjects(shirtObject , longSleeveShirtObject); // => false
 assertEqual(eqObjects(shirtObject , longSleeveShirtObject), false);

37e844f0b689059bf88bb7d24ee69eb3ae4e5e46
mxtchjohnston <mitchjohnston99@gmail.com>
2023-09-22 19:57:17 +0000

    inspect

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,13 @@
 const { isEqual } = require('./isEqual.js');
+const inspect = require('util').inspect;
 
 const assertEqual = function(actual, expected) {
   let passed = isEqual(actual, expected);
 
   if (passed) {
-    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+    console.log(`✅✅✅Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
   } else {
-    console.log(`🚫🚫🚫Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🚫🚫🚫Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
   }
 };
 

89322c6a4a8c12fafe38ab9e9155d7d0a118d3bd
mxtchjohnston <mitchjohnston99@gmail.com>
2023-09-22 19:51:18 +0000

    refactor complete, assert Equal works on all object types of any depth

--- assertEqual.js
+++ assertEqual.js
@@ -1,3 +1,5 @@
+const { isEqual } = require('./isEqual.js');
+
 const assertEqual = function(actual, expected) {
   let passed = isEqual(actual, expected);
 
@@ -8,31 +10,10 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const isEqual = function(value1, value2) {
-  if (Array.isArray(value1) && Array.isArray(value2)) {
-    // Handle arrays by comparing their elements
-    if (value1.length !== value2.length) {
-      return false;
-    }
-
-    for (let i = 0; i < value1.length; i++) {
-      //recurses to check multidimensional values
-      if (!isEqual(value1[i], value2[i])) {
-        return false;
-      }
-    }
-
-    return true;
-  } else {
-    // Handle scalar values by direct comparison
-    return value1 === value2;
-  }
-};
-
-// assertEqual("Lighthouse Labs", "Bootcamp");
-// assertEqual(1, 1);
-// assertEqual([1, 2], [1, 2]);
-// assertEqual([[1, 2], [1, 2]], [[1, 2], [1, 2]]);
-// assertEqual([[1, 2], [3, 4]], [[1, 2], [3, 5]]);
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual([1, 2], [1, 2]);
+assertEqual([[1, 2], [1, 2]], [[1, 2], [1, 2]]);
+assertEqual([[1, 2], [3, 4]], [[1, 2], [3, 5]]);
 
 exports.assertEqual = assertEqual;
---
+++ isEqual.js
@@ -0,0 +1,55 @@
+const eqVal = (val1, val2) => val1 === val2;
+
+const eqArray = (val1, val2) => {
+    if (val1.length !== val2.length) {
+      return false;
+    }
+
+    for (let i = 0; i < val1.length; i++) {
+      //recurses to check multidimensional values
+      if (!isEqual(val1[i], val2[i])) {
+        return false;
+      }
+    }
+    return true;
+};
+
+const eqObjects = (obj1, obj2) => {
+  let entries1 = Object.entries(obj1);
+  let entries2 = Object.entries(obj2);
+
+  if (entries1.length !== entries2.length) {
+    return false;
+  }
+
+  for (let [key, value] of entries1) {
+    if (!isEqual(obj2[key], value)) {
+      return false;
+    }
+  }
+  return true;
+};
+
+
+
+const isEqual = (val1, val2) => {
+  if (typeof val1 === 'object' && typeof val2 === 'object'){
+    if (Array.isArray(val1) && Array.isArray(val2)) {
+      return eqArray(val1, val2);
+    } else {
+      return eqObjects(val1, val2);
+    }
+  } else {
+    return eqVal(val1, val2);
+  }
+}
+
+exports.isEqual = isEqual;
+
+const obj = {a: 1, b: true, c: ['hello', 'world']};
+const obj2 = {c: ['hello','world'], a: 1, b: true};
+
+const combined = {first: obj, second: obj2};
+
+// console.log(isEqual_(obj, obj2));
+// console.log(isEqual_(combined, combined));
